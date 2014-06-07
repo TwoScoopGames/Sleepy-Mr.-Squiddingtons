03758251dfd0e39064fa00e318df6fe48bb01d97
Alex Bezuska <abezuska@gmail.com>
2014-06-07 17:09:57 -0400

    add first player animation

--- html/game.js
+++ html/game.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var canvas = document.getElementById("canvas");
 
 var manifest = {
@@ -20,6 +22,11 @@ var manifest = {
 			"frames": 32,
 			"msPerFrame": 50,
 			"repeatAt": 31
+		},
+		"squid-idle": {
+			"strip": "images/squid-idle-f6.png",
+			"frames": 6,
+			"msPerFrame": 200
 		}
 	}
 };
@@ -42,20 +49,23 @@ function centerText(context, text, offsetX, offsetY) {
 	var y = offsetY | 0;
 	context.fillText(text, x, y);
 }
+var soundSwitch;
 
 function drawIntroOverlay(context, scene) {
 	scene.camera.drawAbsolute(context, function() {
 
 		context.fillStyle = "#fff";
-		context.font = "20px lato";
+		context.font = "20px helvetica";
 		centerText(context, "2014 Two Scoop Games", 0, bounds.bottom - 60);
 
 		if (muteSounds) {
-			var soundSwitch = game.images.get("sound-off");
+			soundSwitch = game.images.get("sound-off");
+			context.drawImage(soundSwitch, (bounds.right - soundSwitch.width), 100);
 		} else {
-			var soundSwitch = game.images.get("sound-on");
+			soundSwitch = game.images.get("sound-on");
+			context.drawImage(soundSwitch, (bounds.right - soundSwitch.width), 100);
 		}
-		context.drawImage(soundSwitch, (bounds.right - soundSwitch.width), 100);
+
 
 	});
 }
@@ -67,17 +77,16 @@ function isInside(container, x, y) {
 		y < container.y + container.height;
 }
 
-function drawCircle(context, color, radius, strokeColor, strokeSize, x, y) {
-	context.beginPath();
-	context.arc(x, y, radius, 0, 2 * Math.PI, false);
-	context.fillStyle = color;
-	context.fill();
-	context.lineWidth = strokeSize;
-	context.strokeStyle = strokeColor;
-	context.stroke();
-}
+// function drawCircle(context, color, radius, strokeColor, strokeSize, x, y) {
+// 	context.beginPath();
+// 	context.arc(x, y, radius, 0, 2 * Math.PI, false);
+// 	context.fillStyle = color;
+// 	context.fill();
+// 	context.lineWidth = strokeSize;
+// 	context.strokeStyle = strokeColor;
+// 	context.stroke();
+// }
 
-var player = new Splat.Entity(bounds.right / 2, bounds.bottom / 2, 32, 32);
 /*=========================================
 				 Scenes 
 ===========================================*/
@@ -99,14 +108,22 @@ game.scenes.add("title", new Splat.Scene(canvas, function() {
 	anim.draw(context, (bounds.right / 2) - (anim.width / 2), (bounds.bottom / 2) - (anim.height / 2));
 }));
 
+
+var playerYSpeed = 1.2;
+var playerXSpeed = 1.1;
+
+
 game.scenes.add("main", new Splat.Scene(canvas, function() {
 		// init
+
 
 		waitingToStart = true;
 
 		this.timers.fadeToBlack = new Splat.Timer(null, 1000, function() {
 			game.scenes.switchTo("main");
 		});
+		this.squidIdle = game.animations.get("squid-idle");
+		this.player = new Splat.AnimatedEntity(25, 25, this.squidIdle.width, this.squidIdle.height, this.squidIdle, 0, 0);
 
 
 	},
@@ -117,12 +134,9 @@ game.scenes.add("main", new Splat.Scene(canvas, function() {
 			//game.sounds.stop("music");
 		}
 
-
-
 		if (waitingToStart) {
 			var soundSwitch = new Splat.Entity((canvas.width - 115), 100, 115, 109);
 			if (isInside(soundSwitch, game.mouse.x, game.mouse.y)) {
-
 				if (game.mouse.consumePressed(0)) {
 					if (muteSounds === true) {
 						muteSounds = false;
@@ -130,26 +144,21 @@ game.scenes.add("main", new Splat.Scene(canvas, function() {
 						muteSounds = true;
 					}
 				}
-
 			} else {
 				if (game.mouse.consumePressed(0)) {
 					if (!muteSounds) {
 						game.sounds.play("button");
 					}
 					setTimeout(function() {
-
 						if (!muteSounds) {
-							game.sounds.play("music", true);
+							//game.sounds.play("music", true);
 						}
 						waitingToStart = false;
-
 					}, 200);
-
 				}
 			}
 
 		}
-
 
 		if (this.timers.fadeToBlack.running) {
 			return;
@@ -157,57 +166,54 @@ game.scenes.add("main", new Splat.Scene(canvas, function() {
 
 
 		if (!waitingToStart) {
-			var playerYSpeed = 1.8;
-			var playerXSpeed = 1.4;
+
+			game.animations.get("squid-idle").move(elapsedMillis);
+
 			if (game.keyboard.isPressed("left") || game.keyboard.isPressed("a")) {
-				player.vx = -playerXSpeed;
+				this.player.vx = -playerXSpeed;
 			}
 			if (game.keyboard.isPressed("right") || game.keyboard.isPressed("d")) {
-				player.vx = playerXSpeed;
+				this.player.vx = playerXSpeed;
 			}
 			if (game.keyboard.isPressed("up") || game.keyboard.isPressed("w")) {
-				player.vy = -playerYSpeed;
+				this.player.vy = -playerYSpeed;
 			}
 			if (game.keyboard.isPressed("down") || game.keyboard.isPressed("s")) {
-				player.vy = playerYSpeed;
+				this.player.vy = playerYSpeed;
 			}
 
 			if (game.keyboard.isPressed) {
-				player.x += player.vx;
-				player.y += player.vy;
+				this.player.x += this.player.vx;
+				this.player.y += this.player.vy;
 			} else {
-				player.x += 0;
-				player.y += 0;
+				this.player.x += 0;
+				this.player.y += 0;
 			}
 
-			player.vy += gravity;
-
-
+			this.player.vy += gravity;
+			if (this.player.x < bounds.left) {
+				this.player.x = bounds.left;
+			}
+			if (this.player.x + this.player.width > bounds.right) {
+				this.player.x = bounds.right - this.player.width;
+			}
+			if (this.player.y < bounds.top) {
+				this.player.y = bounds.top;
+			}
+			if (this.player.y + this.player.height > bounds.bottom) {
+				this.player.y = bounds.bottom - this.player.height;
+			}
 		}
 
 
-
-		if (player.x < bounds.left) {
-			player.x = bounds.left;
-		}
-		if (player.x > bounds.right) {
-			player.x = bounds.right;
-		}
-		if (player.y < bounds.top) {
-			player.y = bounds.top;
-		}
-		if (player.y > bounds.bottom) {
-			player.y = bounds.bottom;
-		}
 
 	},
 
-	function draw(context) {
+	function(context) {
 
-		context.fillStyle = "blue";
+		// draw
+		context.fillStyle = "#728780";
 		context.fillRect(0, 0, canvas.width, canvas.height);
-
-
 
 		if (this.timers.fadeToBlack.running) {
 			return;
@@ -216,12 +222,19 @@ game.scenes.add("main", new Splat.Scene(canvas, function() {
 		if (waitingToStart) {
 			drawIntroOverlay(context, this);
 		} else {
+
+			//console.log("player x: ", this.player.x);
+			this.player.draw(context);
+			// player bounding box
+			//context.strokeRect(this.player.x, this.player.y, this.player.width, this.player.height);
+
+
 			this.camera.drawAbsolute(context, function() {
 				//hud
 				context.fillStyle = "#ffffff";
 				context.font = "20px helvetica";
-				centerText(context, 'hello world', 0, 25);
-				drawCircle(context, "rgba(255,255,255.4)", player.width, "rgba(255,255,255.4)", 0, player.x, player.y);
+				centerText(context, "hello world", 0, 25);
+
 			});
 		}
 
