ce76847d1e49bcef5e9508925b2720302c099dac
Alex Bezuska <abezuska@gmail.com>
2014-10-06 22:47:37 -0400

    add pill collecting, add camera, add some basic platforms

--- html/game.js
+++ html/game.js
@@ -6,11 +6,13 @@ var canvas = document.getElementById("canvas");
 var manifest = {
 	"images": {
 		"sound-off": "images/sound-off.png",
-		"sound-on": "images/sound-on.png"
+		"sound-on": "images/sound-on.png",
+		"bg": "images/bg.png"
 	},
 	"sounds": {
 		"choo": "sound/choo.wav",
-		"button": "sound/menuchange.wav"
+		"button": "sound/menuchange.wav",
+		"pickup": "sound/pickup.wav"
 	},
 	"fonts": {},
 	"animations": {
@@ -29,6 +31,11 @@ var manifest = {
 			"strip": "images/squid-idle-left-f6.png",
 			"frames": 6,
 			"msPerFrame": 200
+		},
+		"pill-anim": {
+			"strip": "images/pill-anim.png",
+			"frames": 6,
+			"msPerFrame": 100
 		}
 	}
 };
@@ -71,12 +78,12 @@ var bubbles = [];
 // 	centerText(context, text, 0, textArea.y + (textArea.height * 0.6));
 // }
 
-// function centerText(context, text, offsetX, offsetY) {
-// 	var textWidth = context.measureText(text).width;
-// 	var x = offsetX + (bounds.right / 2) - (textWidth / 2) | 0;
-// 	var y = offsetY | 0;
-// 	context.fillText(text, x, y);
-// }
+function centerText(context, text, offsetX, offsetY) {
+	var textWidth = context.measureText(text).width;
+	var x = offsetX + (bounds.right / 2) - (textWidth / 2) | 0;
+	var y = offsetY | 0;
+	context.fillText(text, x, y);
+}
 
 
 // function drawIntroOverlay(context, scene) {
@@ -148,12 +155,53 @@ function drawCircle(context, color, radius, strokeColor, strokeSize, x, y) {
 	context.stroke();
 }
 
+// function checkAllObsticles(obsticleArray, entity) {
+// 	for (var i = 0; i < obsticleArray.length; i++) {
+// 		var entityLeft = entity.x;
+// 		var entityRight = entity.x + entity.width;
+// 		var entityTop = entity.y;
+// 		var entityBottom = entity.y + entity.height;
+// 		if (entity.collides(obsticleArray[i])) {
+// 			if (entityRight > obsticleArray[i].x) {
+// 				entity.x = entity.x - 5;
+// 			}
+// 			if (entityLeft > (obsticleArray[i].x + obsticleArray[i].width)) {
+// 				entity.x = (entity.x + entity.width) - 5;
+// 			}
+// 			if (entityBottom < obsticleArray[i].y) {
+// 				entity.y = obsticleArray[i].y - 5;
+// 			}
+// 			if (entityTop < (obsticleArray[i].y + obsticleArray[i].height)) {
+// 				entity.y = (obsticleArray[i].y + obsticleArray[i].height) + 5;
+// 			}
 
-var playerYSpeed = 1.2;
-var playerXSpeed = 1.1;
+// 		}
+// 	}
+// }
+
+var debug = true;
+
+var playerYSpeed = 1.7;
+var playerXSpeed = 1;
 var playerMoving = false;
 
+var levelColiders = [
+	new Splat.Entity(0, -250, canvas.width * 4, 50), //top
+	new Splat.Entity(-20, -250, 20, canvas.height * 4),
+	new Splat.Entity(canvas.width * 4, -250, 20, canvas.height * 4),
+	new Splat.Entity(0, 700, canvas.width * 4, 200)
+];
 
+var platforms = [
+	new Splat.Entity(568, 400, 200, 200),
+	new Splat.Entity(800, 200, 150, 100),
+	new Splat.Entity(1248, 300, 300, 50),
+	new Splat.Entity(2190, 340, 300, 50),
+	new Splat.Entity(3490, 300, 300, 50)
+];
+
+
+var pillsCollected = 0;
 
 /*=========================================
 				 Scenes 
@@ -176,7 +224,7 @@ game.scenes.add("main", new Splat.Scene(canvas, function() {
 		this.player = new Splat.AnimatedEntity((bounds.right / 2) - (this.squidIdle.width / 2), 25, this.squidIdle.width, this.squidIdle.height, this.squidIdle, 0, 0);
 		var self = this;
 
-		this.timers.bubbleTimer = new Splat.Timer(null, 1000, function() {
+		this.timers.bubbleTimer = new Splat.Timer(null, 500, function() {
 			spray(bubbles, {
 					x: self.player.x + (self.player.width * 0.8),
 					y: self.player.y + 35
@@ -186,7 +234,37 @@ game.scenes.add("main", new Splat.Scene(canvas, function() {
 			this.start();
 		});
 		this.timers.bubbleTimer.start();
-		this.rock1 = new Splat.Entity((canvas.width / 2), 0, 50, 50);
+
+
+		this.camera = new Splat.EntityBoxCamera(this.player, 0, 0, canvas.width / 2, canvas.height / 2);
+		this.camera.move = function(elapsedMillis) {
+			if (this.adjusted) {
+				Splat.Entity.prototype.move.call(this, elapsedMillis);
+			} else {
+				Splat.EntityBoxCamera.prototype.move.call(this, elapsedMillis);
+			}
+			this.x = Math.max(0, this.x);
+			this.x = Math.min(canvas.width * 4, this.x);
+			this.y = Math.min(0, this.y);
+			this.y = Math.max(-canvas.height * 4, this.y);
+		};
+
+		this.pillAnim = game.animations.get("pill-anim");
+		this.pills = [
+			new Splat.AnimatedEntity(150, 150, this.pillAnim.width, this.pillAnim.height, this.pillAnim, 0, 0),
+			new Splat.AnimatedEntity(250, 160, this.pillAnim.width, this.pillAnim.height, this.pillAnim, 0, 0),
+			new Splat.AnimatedEntity(350, 170, this.pillAnim.width, this.pillAnim.height, this.pillAnim, 0, 0),
+			new Splat.AnimatedEntity(450, 170, this.pillAnim.width, this.pillAnim.height, this.pillAnim, 0, 0),
+			new Splat.AnimatedEntity(550, 160, this.pillAnim.width, this.pillAnim.height, this.pillAnim, 0, 0),
+			new Splat.AnimatedEntity(650, 150, this.pillAnim.width, this.pillAnim.height, this.pillAnim, 0, 0),
+			new Splat.AnimatedEntity(750, 140, this.pillAnim.width, this.pillAnim.height, this.pillAnim, 0, 0),
+			new Splat.AnimatedEntity(850, 130, this.pillAnim.width, this.pillAnim.height, this.pillAnim, 0, 0),
+			new Splat.AnimatedEntity(950, 120, this.pillAnim.width, this.pillAnim.height, this.pillAnim, 0, 0),
+		];
+		for (var pr = 0; pr < this.pills.length; pr++) {
+			this.pills[pr].rotation = (Math.random() * -3.14) + 3.14;
+		}
+
 
 	},
 	function(elapsedMillis) {
@@ -240,22 +318,22 @@ game.scenes.add("main", new Splat.Scene(canvas, function() {
 		this.player.vy += gravity;
 
 		//Player restricted to bounds
-		if (this.player.x < bounds.left) {
-			this.player.x = bounds.left;
-			this.player.vx *= -0.5;
-		}
-		if (this.player.x + this.player.width > bounds.right) {
-			this.player.x = bounds.right - this.player.width;
-			this.player.vx *= -0.5;
-		}
-		if (this.player.y < bounds.top) {
-			this.player.y = bounds.top;
-			this.player.vy *= -0.5;
-		}
-		if (this.player.y + this.player.height > bounds.bottom) {
-			this.player.y = bounds.bottom - this.player.height;
-			this.player.vy *= -0.5;
-		}
+		// if (this.player.x < bounds.left) {
+		// 	this.player.x = bounds.left;
+		// 	this.player.vx *= -0.5;
+		// }
+		// if (this.player.x + this.player.width > bounds.right) {
+		// 	this.player.x = bounds.right - this.player.width;
+		// 	this.player.vx *= -0.5;
+		// }
+		// if (this.player.y < bounds.top) {
+		// 	this.player.y = bounds.top;
+		// 	this.player.vy *= -0.5;
+		// }
+		// if (this.player.y + this.player.height > bounds.bottom) {
+		// 	this.player.y = bounds.bottom - this.player.height;
+		// 	this.player.vy *= -0.5;
+		// }
 
 
 		//change animations per direction
@@ -267,24 +345,71 @@ game.scenes.add("main", new Splat.Scene(canvas, function() {
 		}
 
 
+		this.player.solveCollisions(levelColiders);
+		this.player.solveCollisions(platforms);
+		//checkAllObsticles(levelColiders, this.player);
+
+		this.pillAnim.move(elapsedMillis);
+
+		for (var pill = 0; pill < this.pills.length; pill++) {
+			if (this.player.collides(this.pills[pill])) {
+				game.sounds.play("pickup");
+				this.pills.splice(pill, 1);
+				pillsCollected++;
+			}
+		}
+
+
 	},
 
 	function(context) {
 		// draw
-		context.fillStyle = "#C3E3B3";
-		context.fillRect(0, 0, canvas.width, canvas.height);
+		this.camera.drawAbsolute(context, function() {
+
+			context.drawImage(game.images.get("bg"), 0, -canvas.width / 2);
+
+		});
+
 
 		//draw the player
 		this.player.draw(context);
+		drawParticles(context, bubbles);
+
+
+		for (var pill = 0; pill < this.pills.length; pill++) {
+			//context.save();
+			//context.rotate(this.pills[pill].rotation);
+			this.pills[pill].draw(context);
+			//context.restore();
+		}
+
 
 		//var self = this;
+		// this.camera.drawAbsolute(context, function() {
+
+		// 	// 	//hud
+		// 	// 	drawTextArea(context, "all was well, until one day...");
+
+
+		// });
+
+		if (debug === true) {
+			//draw barrier (debug)
+			context.strokeStyle = "red";
+			for (var i = 0; i < levelColiders.length; i++) {
+				context.strokeRect(levelColiders[i].x, levelColiders[i].y, levelColiders[i].width, levelColiders[i].height);
+			}
+			for (var p = 0; p < platforms.length; p++) {
+				context.strokeRect(platforms[p].x, platforms[p].y, platforms[p].width, platforms[p].height);
+			}
+
+		}
 		this.camera.drawAbsolute(context, function() {
-			drawParticles(context, bubbles);
-			// 	//hud
-			// 	drawTextArea(context, "all was well, until one day...");
-
-
+			context.fillStyle = "white";
+			context.font = "20px helvetica";
+			centerText(context, pillsCollected, 50, 50);
 		});
+
 
 	}));
 
